a21fb68f63f63c1b942bc374f0fdd807ba4622c7
41vi4p <dav7.digi@yahoo.com>
2025-02-06 06:22:04 +0530

    fire

--- email-guard/next.config.ts
+++ email-guard/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  protocol: "https",
+  hostname: "firebasestorage.googleapis.com",
+  port: "",
+  pathname: "/**",
 };
 
 export default nextConfig;
